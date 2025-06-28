6b6a854ef5f48c0d7a23680e0d07275861052511
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2025-06-28 18:06:56 +0100

    Use default adjustments rather than map

--- MusicLib/Functions/fitToChordFunction.cpp
+++ MusicLib/Functions/fitToChordFunction.cpp
@@ -23,6 +23,7 @@ namespace {
 
     const ChordDegreeAdjustments chordDegreeAdjustments = {
         // How each chord requires and/or modifies certain degrees of the scale.
+        // Degree 1 and 5 don't need to be specified if default.
         // The adjustments are in semitones.
         {bw_music::ChordType::Value::M, {{3, 0}}},
         {bw_music::ChordType::Value::M6, {{3, 0}, {6, 0}}},
@@ -57,7 +58,7 @@ namespace {
         {bw_music::ChordType::Value::_1p8, {}}, // TODO Unsure how to handle this. For now, it is a no-op.
         {bw_music::ChordType::Value::_1p5, {}}, // TODO Unsure how to handle this. For now, it is a no-op.
         {bw_music::ChordType::Value::sus4, {{3, 1}}},
-        {bw_music::ChordType::Value::_1p2p5, {{3, -2}}},
+        {bw_music::ChordType::Value::_1p2p5, {{3, -2}}}, // Assuming this should be interpreted as a sus2 chord.
         {bw_music::ChordType::Value::b5, {{5, -1}}},
         {bw_music::ChordType::Value::mM7b5, {{3, -1}, {5, -1}, {7, 0}}},
         {bw_music::ChordType::Value::m6_9, {{3, -1}, {6, 0}, {9, 0}}}};
@@ -66,11 +67,7 @@ namespace {
     // Degree  1     2     3  4     5     6     7
     // Degree  8     9    10 11    12    13    14
     // Index   0  1  2  3  4  5  6  7  8  9 10 11
-    const std::array<unsigned int, 14> degreeToPitchIndex = {0, 2, 4, 5, 7, 9, 11, 0, 2, 4, 5, 7, 9, 11};
-
-    // Some chromatic notes are adjusted by default, if they have not be otherwise adjusted by the algorithm.
-    // 99 is just a sentinel used for assertions.
-    const std::array<unsigned int, 12> defaultChromaticPitchMap = {99, 0, 99, 3, 99, 99, 7, 99, 7, 99, 10, 99};
+    const std::array<unsigned int, 15> degreeToPitchIndex = {999, 0, 2, 4, 5, 7, 9, 11, 0, 2, 4, 5, 7, 9, 11};
 
     using PitchIndexMap = std::array<unsigned int, 12>;
 
@@ -81,7 +78,14 @@ namespace {
         PitchIndexMap pitchIndexMap = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
         // Apply adjustments from the modifiers
-        const auto& adjustments = chordDegreeAdjustments.at(chordType);
+        const auto it = chordDegreeAdjustments.find(chordType);
+        assert(it != chordDegreeAdjustments.end() && "Chord type not found in adjustments map");
+        DegreeAdjustment adjustments = it->second;
+
+        // Apply defaults
+        adjustments[1] = 0; // Degree 0 (root) is always present.
+        adjustments.insert_or_assign(5, 0); // Degree 5 is assumed unless specified.
+
         for (const auto& [degree, adjustment] : adjustments) {
             assert(degree < degreeToPitchIndex.size() && "Degree out of range");
             const unsigned int sourcePitchIndex = degreeToPitchIndex[degree];
@@ -90,10 +94,7 @@ namespace {
             assert(targetPitchIndex < pitchIndexMap.size() && "Target pitch class out of range");
             pitchIndexMap[sourcePitchIndex] = targetPitchIndex;
             gravityMap[targetPitchIndex] = 1;
-            for (unsigned int index = sourcePitchIndex; index != targetPitchIndex; index += sgn(adjustment)) {
-                pitchIndexMap[sourcePitchIndex] = targetPitchIndex;
-                gravityMap[index] = -1;
-            }
+            gravityMap[sourcePitchIndex] = -1;
         }
         // For remaining chromatic notes, if either neighbouring note is a target of a modifier, pull it towards the
         // target.
@@ -108,12 +109,6 @@ namespace {
                     // This is left-biased, but I don't think it matters much.
                     pitchIndexMap[index] = (gravityMap[leftNeighbour] > gravityMap[rightNeighbour]) ? leftNeighbour
                                                                                                       : rightNeighbour;
-                }
-                else {
-                    // TODO Provide a policy so the user can opt out of this behaviour.
-                    // If neither neighbour is a target, use the default chromatic pitch map.
-                    pitchIndexMap[index] = defaultChromaticPitchMap[index];
-                    assert(pitchIndexMap[index] < 12 && "Default chromatic pitch index out of range");
                 }
             }
         }
