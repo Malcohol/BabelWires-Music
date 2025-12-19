f690cd01222ba2d04fe903ab5e3108fc5e0c5c7d
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2025-12-19 09:09:17 +0000

    getSetOfChordTypesFunction

---
+++ MusicLib/Functions/getSetOfChordTypesFunction.cpp
@@ -0,0 +1,24 @@
+/**
+ * Function which determines the set of chords used by a chord track.
+ *
+ * (C) 2025 Malcolm Tyrrell
+ *
+ * Licensed under the GPLv3.0. See LICENSE file.
+ */
+#include <MusicLib/Functions/getSetOfChordTypesFunction.hpp>
+
+#include <MusicLib/Types/Track/track.hpp>
+#include <MusicLib/Types/Track/TrackEvents/chordEvents.hpp>
+
+std::set<bw_music::ChordType::Value> bw_music::getSetOfChordTypesFunction(const Track& chordTrack) {
+    std::set<ChordType::Value> chordTypes;
+
+    // Iterate through the chord track and collect unique chord types.
+    for (const auto& event : chordTrack) {
+        if (const auto chordEvent = event.as<ChordOnEvent>()) {
+            chordTypes.insert(chordEvent->m_chord.m_chordType);
+        }
+    }
+
+    return chordTypes;
+}
---
+++ MusicLib/Functions/getSetOfChordTypesFunction.hpp
@@ -0,0 +1,21 @@
+/**
+ * Function which determines the set of chords used by a chord track.
+ *
+ * (C) 2025 Malcolm Tyrrell
+ *
+ * Licensed under the GPLv3.0. See LICENSE file.
+ **/
+#pragma once
+
+#include <BabelWiresLib/TypeSystem/registeredType.hpp>
+
+#include <MusicLib/Types/Track/track.hpp>
+#include <MusicLib/chord.hpp>
+
+#include <set>
+
+namespace bw_music {
+    /// Get the set of chord types used in the given chord track.
+    std::set<ChordType::Value> getSetOfChordTypesFunction(const Track& chordTrack);
+
+} // namespace bw_music
