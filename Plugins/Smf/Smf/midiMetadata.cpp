9c8e497c2786f429f8508c293203e0a98c3d6ee7
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2026-06-20 17:11:02 +0100

    StringType -> TextType

--- Plugins/Smf/Smf/midiMetadata.cpp
+++ Plugins/Smf/Smf/midiMetadata.cpp
@@ -12,7 +12,7 @@
 #include <MusicLib/Types/tempo.hpp>
 
 #include <BabelWiresLib/TypeSystem/typeSystem.hpp>
-#include <BabelWiresLib/Types/String/stringType.hpp>
+#include <BabelWiresLib/Types/String/textType.hpp>
 
 #include <BaseLib/Identifiers/registeredIdentifier.hpp>
 
@@ -23,7 +23,7 @@ smf::MidiMetadata::MidiMetadata(const babelwires::TypeSystem& typeSystem)
            {BW_SHORT_ID("Tempo", "Tempo", "3ef804e9-e34a-4a25-b6bf-ce7597d9d90b"),
             bw_music::Tempo::getThisIdentifier(), babelwires::RecordType::Optionality::optionalDefaultInactive},
            {BW_SHORT_ID("Name", "Name", "c2e4910f-d006-4a93-97a7-ae5973157ec8"),
-            babelwires::StringType::getThisIdentifier(), babelwires::RecordType::Optionality::optionalDefaultInactive},
+            babelwires::TextType::getThisIdentifier(), babelwires::RecordType::Optionality::optionalDefaultInactive},
            {BW_SHORT_ID("CopyR", "Copyright", "a59dc914-d060-4f03-be83-5804fc4d6b6a"),
-            babelwires::StringType::getThisIdentifier(),
+            babelwires::TextType::getThisIdentifier(),
             babelwires::RecordType::Optionality::optionalDefaultInactive}}) {}
